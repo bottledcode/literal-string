503edac82643d62223055be9d878e9a3a0ba7f5a
Robert Landers <landers.robert@gmail.com>
2024-03-10 16:34:27 +0100

    add strict tests

---
+++ tests/Unit/StrictStringTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+it('is equal to itself', function () {
+    expect(LiteralString::from("value"))->toBe(LiteralString::from("value"));
+});
+
+it('handles a < b', function () {
+    expect(LiteralString::from('a') < LiteralString::from('b'))->toBeTrue()
+        ->and(LiteralString::from('b') > LiteralString::from('a'))->toBeTrue();
+});
+
+it('handles concatenation', function () {
+    expect(LiteralString::from('a') . 'b')->toBe('ab');
+});
